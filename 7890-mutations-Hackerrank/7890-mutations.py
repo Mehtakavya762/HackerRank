d35e01f0a06418b1ed8a17484296d201d1f52845
KAVYA MEHTA <mehtakavya762@gmail.com>
2026-09-11 12:14:10 +0530

    Force committed solution - Time: O(n), Space: O(n)

---
+++ 7890-mutations-Hackerrank/7890-mutations.py
@@ -0,0 +1,12 @@
+def mutate_string(string, position, character):
+    chars = list(string)
+    chars[position] = character
+    return "".join(chars)
+ 
+
+
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
