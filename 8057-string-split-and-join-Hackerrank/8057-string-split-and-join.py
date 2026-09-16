e5dabb0361849956625aa2dc7fcf331cf3e38228
KAVYA MEHTA <mehtakavya762@gmail.com>
2026-09-16 12:41:50 +0530

    Force committed solution - Time: O(n), Space: O(n)

---
+++ 8057-string-split-and-join-Hackerrank/8057-string-split-and-join.py
@@ -0,0 +1,11 @@
+def split_and_join(line):
+    # write your code here
+    line=line.split(" ")
+    line="-".join(line)
+    return line
+  
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
