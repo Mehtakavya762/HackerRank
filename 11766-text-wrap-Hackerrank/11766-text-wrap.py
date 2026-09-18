fdff63341fefa811915c2cb9497fb261c1df3bfb
KAVYA MEHTA <mehtakavya762@gmail.com>
2026-09-18 10:55:29 +0530

    Force committed solution - Time: O(n), Space: O(n)

---
+++ 11766-text-wrap-Hackerrank/11766-text-wrap.py
@@ -0,0 +1,13 @@
+
+
+def wrap(string, max_width):
+    
+    result=textwrap.fill(string,max_width)
+    
+    return result
+
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
